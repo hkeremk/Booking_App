cb0e3515e5ac0fa3baa263559e449be1ab115eb4
hkeremk <hkeremk@users.noreply.github.com>
2022-09-28 20:44:59 +0300

    variables added

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Wilkommen")
+	var eventName = "Go Events"
+	const eventTickets = 100
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", eventName, "booking application")
+	fmt.Println("We have total of", eventTickets, "tickets and", remainingTickets, "are still avaliable")
+	fmt.Println("Get your tickets here to attend")
 }
