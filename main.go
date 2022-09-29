2b5740740d9a10022e10cced6e2608a3d34fe30f
hkeremk <hkeremk@users.noreply.github.com>
2022-09-29 15:57:05 +0300

    pointers added

--- main.go
+++ main.go
@@ -4,10 +4,31 @@ import "fmt"
 
 func main() {
 	var eventName = "Go Events"
-	const eventTickets = 100
-	var remainingTickets = 50
+	const eventTickets int = 100
+	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to", eventName, "booking application")
-	fmt.Println("We have total of", eventTickets, "tickets and", remainingTickets, "are still avaliable")
+	fmt.Printf("Welcome to %v booking application\n", eventName)
+	fmt.Printf("We have total of %v tickets and %v are still avaliable\n", eventTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, eventTickets)
 }
